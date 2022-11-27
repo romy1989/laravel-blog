e08f3d7c1112fd533875803a3c207aea0ec829e2
Romy89 <romy@miles.delivery>
2022-11-27 13:18:27 +0100

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
